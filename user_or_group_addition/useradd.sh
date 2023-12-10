746081a911439b7ba1dfb35044b394a21698a973
root <root@ip-172-31-36-56.ec2.internal>
2023-12-10 10:44:01 +0000

    changes

--- user_or_group_addition/useradd.sh
+++ user_or_group_addition/useradd.sh
@@ -1,9 +1,9 @@
-if [ '#$' -eq 0]; then 
+if [ "$#" -eq 0]; then 
 	echo "There are no arguments!"
 	exit 1
 fi
 
-for arg in "@$"; do
-	sudo useradd "@arg"
-	echo "User @arg has been added"
+for arg in "$@"; do
+	sudo useradd "$arg"
+	echo "User $arg has been added"
 done
